87d74e5b328348733a7bc3590d67a8ae95a52316
byron <dhhnk4947@gmail.com>
2026-09-07 15:30:36 +0900

    Replace public with web-mobile build

--- assets/scripts/Tea/Util/EventManager.ts
+++
@@ -1,9 +0,0 @@
-import { EventBase } from "./EventBase";
-import { Singleton } from "./SingletonFactory";
-
-/**
- * 事件管理器新建模板
- * export class {ClassName} extends EventBase{}
- */
-@Singleton()
-export default class EventManager extends EventBase{}
