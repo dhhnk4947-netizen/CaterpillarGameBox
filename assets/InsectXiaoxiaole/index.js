277b3af7609ea4faab1c909dbf46df0c3f12e341
byron <dhhnk4947@gmail.com>
2026-09-15 21:24:57 +0900

    11

--- assets/InsectXiaoxiaole/index.js
+++ assets/InsectXiaoxiaole/index.js
@@ -1,8 +1,4 @@
-System.register("chunks:///_virtual/InsectXiaoxiaole", [], function () {
-  return {
-    execute: function () {}
-  };
-});
+System.register("chunks:///_virtual/InsectXiaoxiaole",[],(function(){return{execute:function(){}}}));
 
 (function(r) {
   r('virtual:///prerequisite-imports/InsectXiaoxiaole', 'chunks:///_virtual/InsectXiaoxiaole'); 
